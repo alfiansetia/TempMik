9bdf7e641ca9a8e03936a0566f9c9f474ead823e
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2021-02-27 16:16:20 +0700

    add assets

---
+++ v1/assets/js/data.js
@@ -0,0 +1,22 @@
+var data = {
+    'company': '@NET'
+}
+var dataLogin = {
+    'title1': 'Please Login First',
+    'priceList': 'Price List',
+    'address': 'Voucher bisa dibeli di Depan Musholla Darrul Athfal Dk. Ngetrep Ds. Kacangan.',
+    'phone': '0823-2412-9752'
+}
+var dataAlogin = {
+    'title': 'Success',
+    'alert': 'Login Success'
+}
+var dataStatus = {
+    'title': 'Status',
+    'title1': 'Welcome',
+    'alert': 'Are You Sure To Logout?'
+}
+var dataLogout = {
+    'title1': 'Good Bye',
+    'alert': 'Are You Sure To Logout?'
+}
